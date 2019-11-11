79c8f653f258adabb3b4265e209596c9869ec313
Elias Tooloee <elitooloee@gmail.com>
2019-11-11 21:28:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,3 +15,22 @@ def map_to_square(array)
   array.map { |element| element**2 }
 end
 
+def reduce_to_total(array, starting_point=0)
+  array.reduce(starting_point) { |sum, num| sum+num}
+end
+
+def reduce_to_all_true(array)
+  array.reduce { |beTrue, beFalse| beTrue && beFalse}
+end
+
+def reduce_to_any_true(array)
+  i=0
+  while i<array.length do
+    if array[i]==true
+      return true
+    end
+    i+=1
+  end
+  return false
+end  
+  
